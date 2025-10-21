b2ca34c679907df68bf80c19e04ff031cf069d0a
Paula Díaz <114782303+pauladiiaz@users.noreply.github.com>
2025-10-21 11:16:20 +0100

    Refactor collision color change logic
    Removed unused Renderer initialization and added color change on collision.

--- scritps/CollisionColorChange.cs
+++ scritps/CollisionColorChange.cs
@@ -2,21 +2,13 @@ using UnityEngine;
 
 public class CollisionColorChange : MonoBehaviour
 {
-    private Renderer rend;
-
-    private void Start()
-    {
-        rend = GetComponent<Renderer>();
-        if (rend == null)
-            Debug.LogWarning("No Renderer found on " + gameObject.name);
-    }
-
     private void OnCollisionEnter(Collision collision)
     {
         // Muestra en consola el nombre del objeto con el que colisiona
         Debug.Log(gameObject.name + " colisionó con " + collision.gameObject.name);
 
         // Cambia el color a un color aleatorio
+        Renderer rend = collision.gameObject.GetComponent<Renderer>();
         if (rend != null)
         {
             rend.material.color = new Color(Random.value, Random.value, Random.value);
